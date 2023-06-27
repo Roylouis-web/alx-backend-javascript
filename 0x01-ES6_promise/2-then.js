40aa0423952f422857e08ec302d5dc1e86b3bf66
Roylouis-web <powellleroy488@gmail.com>
2023-06-27 18:42:48 +0100

    All mandatory and advance task completed

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,6 @@
 export default function handleResponseFromAPI(promise) {
   promise
-    .then(() => ({ status: 200, body: 'success' }))
-    .catch(() => new Error())
+    .then((data) => ({ status: 200, body: 'success' }))
+    .catch((error) => new Error())
     .finally(() => console.log("Got a response from the API"));
 }
