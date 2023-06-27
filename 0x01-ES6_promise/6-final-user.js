06ca91b32d7362802c66f9d8b50227b5399afdc7
Roylouis-web <powellleroy488@gmail.com>
2023-06-27 23:58:37 +0100

    All mandatory and advance task completed

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,7 +4,7 @@ import uploadPhoto from "./5-photo-reject";
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
+    uploadPhoto(fileName)
   ]).then((values) => {
     const arr = [];
     values.forEach((element) => {
