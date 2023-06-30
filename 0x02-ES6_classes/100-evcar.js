2621c2a207c87303a2e349bbc31ff970fd920eec
Roylouis-web <powellleroy488@gmail.com>
2023-06-30 03:08:44 +0000

    All done

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,4 +1,5 @@
 import Car from './10-car';
+/* eslint  class-methods-use-this: ['error', { 'exceptMethods': ['cloneCar'] }] */
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
