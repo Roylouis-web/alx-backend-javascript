bf48b223e34555bf6c13075386d0d29e8c10c9c5
Roylouis-web <powellleroy488@gmail.com>
2023-07-06 05:17:04 +0100

    All done with my tasks

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -8,16 +8,11 @@ interface MinorCredits {
   brand: string;
 }
 
-const sumMajorCredits = (subject1: MajorCredits, subject2: MajorCredits): MajorCredits => {
-  return {
-    credits: subject1.credits + subject2.credits,
-    brand: 'subject1',
-  };
+const sumMajorCredits = (subject1: MajorCredits, subject2: MajorCredits): number => {
+   return subject1.credits + subject2.credits;
+  
 };
 
-const sumMinorCredits = (subject1: MinorCredits, subject2: MinorCredits): MinorCredits => {
-  return {
-    credits: subject1.credits + subject2.credits,
-    brand: 'subject2',
-  };
+const sumMinorCredits = (subject1: MinorCredits, subject2: MinorCredits): number => {
+   return subject1.credits + subject2.credits;
 };
