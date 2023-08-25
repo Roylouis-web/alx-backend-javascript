6de391ebe9227affed96ce1b9ccaae94ea92b386
Roylouis-web <powellleroy488@gmail.com>
2023-08-25 02:48:19 +0000

    All done

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -2,13 +2,14 @@ const request = require('request');
 const { expect } = require('chai');
 const { describe, it } = require('mocha');
 
-describe('GET /cart/:id', () => {
+describe('Cart page', () => {
   it('tests /cart/:id with correct request param', () => {
     request('http://localhost:7865/cart/12', (error, response, body) => {
       if (error) {
         console.log(error);
       }
       expect(response.statusCode).to.equal(200);
+      expect(response.complete).to.equal(true);
       expect(response.headers['content-type']).to.equal('text/plain');
       expect(body).to.equal('Payment methods for cart 12');
     });
@@ -20,10 +21,13 @@ describe('GET /cart/:id', () => {
         console.log(error);
       }
       expect(response.statusCode).to.equal(404);
+      expect(response.complete).to.equal(true);
       expect(response.headers['content-type']).to.equal('text/html; charset=utf-8');
     });
   });
+});
 
+describe('Available payments', () => {
   it('tests GET /available_payments', () => {
     request('http://localhost:7865/available_payments', (error, response, body) => {
       if (error) {
@@ -36,11 +40,14 @@ describe('GET /cart/:id', () => {
         }
       };
       expect(response.statusCode).to.equal(200);
+      expect(response.complete).to.equal(true);
       expect(response.headers['content-type']).to.equal('application/json; charset=utf-8');
       expect(JSON.parse(body)).to.deep.equal(obj);
     });
   });
+});
 
+describe('Login', () => {
   it('tests POST /login', () => {
     const body = { userName: 'Betty' };
     request.post('http://localhost:7865/login', { json: body }, (error, response, body) => {
