1b04ed7dac9e582bf649b7b98c5e8e0f46dc9bd2
Roylouis-web <powellleroy488@gmail.com>
2023-08-25 14:05:14 +0000

    All done

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -2,6 +2,20 @@ const request = require('request');
 const { expect } = require('chai');
 const { describe, it } = require('mocha');
 
+describe('Index page', () => {
+  it('tests the home page end point', () => {
+    request('http://localhost:7865', (error, response, body) => {
+      if (error) {
+        console.log(error);
+      }
+      expect(response.statusCode).to.equal(200);
+      expect(response.complete).to.equal(true);
+      expect(response.headers['content-type']).to.equal('text/plain');
+      expect(body).to.equal('Welcome to the payment system');
+    });
+  });
+});
+
 describe('Cart page', () => {
   it('tests /cart/:id with correct request param', () => {
     request('http://localhost:7865/cart/12', (error, response, body) => {
@@ -14,7 +28,9 @@ describe('Cart page', () => {
       expect(body).to.equal('Payment methods for cart 12');
     });
   });
+});
 
+describe('Cart Page with wrong param', () => {
   it('tests GET /cart/:id with incorrect request param', () => {
     request('http://localhost:7865/cart/hello', (error, response, body) => {
       if (error) {
