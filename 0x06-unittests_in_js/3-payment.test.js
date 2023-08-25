4b2579c565016c48c22ebd0f2f78a6bc11bb9d53
Roylouis-web <powellleroy488@gmail.com>
2023-08-25 00:50:20 +0000

    All done

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,5 +1,5 @@
 const { describe, it } = require('mocha');
-const { assert, expect } = require('chai');
+const assert = require('assert');
 const { spy } = require('sinon');
 const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./3-payment');
@@ -14,11 +14,11 @@ describe('sendPaymentRequestToApi', () => {
   });
 
   it('validate the usage of the Utils function', () => {
-    expect(Utils.calculateNumber('SUM', 100, 20)).to.equal(120);
-    expect(Utils.calculateNumber('SUM', 10.5, 20.5)).to.equal(32);
-    expect(Utils.calculateNumber('SUBTRACT', 10, 20)).to.equal(-10);
-    expect(Utils.calculateNumber('SUBTRACT', 1.7, 9)).to.equal(-7);
-    expect(Utils.calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
-    expect(Utils.calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+    assert.strictEqual(Utils.calculateNumber('SUM', 100, 20), 120);
+    assert.strictEqual(Utils.calculateNumber('SUM', 10.5, 20.5), 32);
+    assert.strictEqual(Utils.calculateNumber('SUBTRACT', 10, 20), -10);
+    assert.strictEqual(Utils.calculateNumber('SUBTRACT', 1.7, 9), -7);
+    assert.strictEqual(Utils.calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+    assert.strictEqual(Utils.calculateNumber('DIVIDE', 1.4, 0), 'Error');
   });
 });
