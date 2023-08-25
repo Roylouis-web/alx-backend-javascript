566d495c8109b15cbb6f4e863f9800bc24e211d0
Roylouis-web <powellleroy488@gmail.com>
2023-08-25 02:03:52 +0000

    All done

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -13,4 +13,15 @@ describe('GET /', () => {
       expect(body).to.equal('Welcome to the payment system');
     });
   });
+  
+  it('tests the home page end point POST', () => {
+    request.put('http://localhost:7865', (error, response, body) => {
+      if (error) {
+        console.log(error);
+      }
+      
+      expect(response.statusCode).to.equal(404);
+      expect(response.headers['content-type']).to.equal('text/html; charset=utf-8');
+    });
+  });
 });
