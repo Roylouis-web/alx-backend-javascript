4f14f372ef35de0cd1f8fb80fac3e4d8bc13d0de
Roylouis-web <powellleroy488@gmail.com>
2023-08-25 00:31:47 +0000

    All done

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,18 +1,25 @@
 const { expect } = require('chai');
 const { describe, it } = require('mocha');
-const calculateNumber = require('./1-calcul');
+const calculateNumber = require('./2-calcul_chai');
 
 describe('Test rounded sum', () => {
   it('test rounded sum for positive non-decimal numbers', () => {
-    expect(calculateNumber('SUM', 5, 3)).to.equal(8);
+    expect(calculateNumber('SUM', 1, 3)).to.equal(4);
   });
-  it('test rounded difference for positive decimal numbers', () => {
-    expect(calculateNumber('SUBTRACT', 5.9, 3.3)).to.equal(3);
+
+  it('test rounded sum for positive non-decimal numbers', () => {
+    expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
   });
-  it('test rounded division for negative non-decimal numbers', () => {
-    expect(calculateNumber('DIVIDE', -6, -4)).to.equal(1.5);
+
+  it('test rounded difference for positive decimal numbers and whole number', () => {
+    expect(calculateNumber('SUBTRACT', 1, 3.7)).to.equal(-3);
   });
-  it('test rounded division for zero digits', () => {
-    expect(calculateNumber('DIVIDE', 0, 0)).to.equal('Error');
+
+  it('test rounded division for positive decimal numbers', () => {
+    expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+  });
+
+  it('test error output for zero division', () => {
+    expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
   });
 });
