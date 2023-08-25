d4d4f0df643d762f877ece9734cfda0630bb51d0
Roylouis-web <powellleroy488@gmail.com>
2023-08-25 01:24:59 +0000

    All done

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,12 +1,12 @@
 const { describe, it } = require('mocha');
 const assert = require('assert');
-const { spy } = require('sinon');
+const sinon = require('sinon');
 const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', () => {
   it('checks for right output of the Utils.calculateNumber method', () => {
-    const UtilsSpy = spy(Utils, 'calculateNumber');
+    const UtilsSpy = sinon.spy(Utils, 'calculateNumber');
 
     sendPaymentRequestToApi(50, 24.52);
 
