dc2721e9ff7ca4f3f0145ab692c057a51ba8089d
Roylouis-web <powellleroy488@gmail.com>
2023-08-25 00:14:43 +0000

    All done

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -4,26 +4,22 @@ const calculateNumber = require('./0-calcul');
 
 describe('Test rounded sum', () => {
   it('test rounded sum for positive non-decimal numbers', () => {
-    assert.strictEqual(calculateNumber(5, 3), 8);
+    assert.strictEqual(calculateNumber(1, 3), 4);
+  });
+
+  it('test rounded sum for positive decimal numbers and whole number', () => {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
   });
 
   it('test rounded sum for positive decimal numbers', () => {
-    assert.strictEqual(calculateNumber(5.9, 3.3), 9);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
   });
 
-  it('test rounded sum for negative non-decimal numbers', () => {
-    assert.strictEqual(calculateNumber(-6, -4), -10);
-  });
-
-  it('test rounded sum for negative decimal numbers', () => {
-    assert.strictEqual(calculateNumber(-34.78, -24.89), -60);
+  it('test rounded sum for positive decimal numbers', () => {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
 
   it('test for a decimal and a whole number', () => {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
-  }
-
-  it('test rounded sum for zero digits', () => {
-    assert.strictEqual(calculateNumber(0, 0), 0);
   });
 });
